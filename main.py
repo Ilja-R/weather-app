abba21fbda97c85dc7bd13253d8d4728dc6e6582
ilja.robalkin <ilja.robalkin@taltech.ee>
2024-12-06 22:31:58 +0200

    Add base functionality

---
+++ main.py
@@ -0,0 +1,37 @@
+import weather
+import asyncio
+
+
+async def main():
+    while True:
+        user_input = input("Enter key or 'q' to quit: ")
+
+        if user_input.strip().lower() == "q":
+            break
+        else:
+            print(f"Getting weather for {user_input}\n")
+            cities = user_input.split(",")
+            city_weather = await weather.get_weather(cities)
+            pretty_print(city_weather)
+
+    print("Goodbye!")
+
+
+def pretty_print(weather_data: tuple):
+    for data in weather_data:
+        print(f"City: {data['key']}")
+
+        if 'error' in data:
+            print(f"    Error: {data['error']}")
+            print()
+        else:
+            print(f"    Temperature: {data['temp']}°C")
+            print(f"    Temperature: {data['temp_fahrenheit']}°F")
+            print(f"    Weather: {data['description']}")
+            print(f"    Humidity: {data['humidity']}%")
+            print(f"    Wind Speed: {data['wind_speed']}m/s")
+            print()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
